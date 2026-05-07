fedc28ebe264e98e28d7490f7cb8f52bd1f66070
Josef Friedrich <josef@friedrich.rocks>
2026-05-07 09:56:50 +0200

    Implement jump through the platforms

--- src/main/java/froggy_jump/Platform.java
+++ src/main/java/froggy_jump/Platform.java
@@ -1,13 +1,34 @@
 package froggy_jump;
 
 import pi.actor.Rectangle;
+import pi.event.CollisionEvent;
+import pi.event.CollisionListener;
 
-class Platform extends Rectangle
+class Platform extends Rectangle implements CollisionListener<Frog>
 {
     public Platform(double width, double height)
     {
         super(width, height);
         makeStatic();
         color("brown");
+        addCollisionListener(Frog.class, this);
+
+    }
+
+    @Override
+    public void onCollision(CollisionEvent<Frog> event)
+    {
+        Frog frog = event.colliding();
+        if (frog.y() < y())
+        {
+            event.ignoreCollision();
+            frog.jumpEnabled(false);
+        }
+    }
+
+    @Override
+    public void onCollisionEnd(CollisionEvent<Frog> event)
+    {
+        event.colliding().jumpEnabled(true);
     }
 }
